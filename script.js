25122d3b32f5f1d8012e54191b6f45008334286b
Sofia Hudson <sofia-sama@hotmail.com>
2023-04-19 10:28:44 +0200

    add DOM to JS badly

--- script.js
+++ script.js
@@ -1,26 +1,36 @@
+let playerCard = document.querySelector(".player-Card");
+let computerCard = document.querySelector(".computer-Card");
+let higher = document.querySelector(".higher");
+const lower = document.querySelector(".lower");
+
+let playerCard = "";
+let computerCard = "";
+let higher = "";
+let lower = "";
+
 const carta = [
-  { valor: 1, name: "as" },
-  { valor: 2, name: "dos" },
-  { valor: 3, name: "tres" },
-  { valor: 4, name: "cuatro" },
-  { valor: 5, name: "cinco" },
-  { valor: 6, name: "seis" },
-  { valor: 7, name: "siete" },
-  { valor: 8, name: "ocho" },
-  { valor: 9, name: "nueve" },
-  { valor: 10, name: "diez" },
-  { valor: 11, name: "jota" },
-  { valor: 12, name: "reina" },
-  { valor: 13, name: "rey" },
+  { value: 1, name: "as" },
+  { value: 2, name: "dos" },
+  { value: 3, name: "tres" },
+  { value: 4, name: "cuatro" },
+  { value: 5, name: "cinco" },
+  { value: 6, name: "seis" },
+  { value: 7, name: "siete" },
+  { value: 8, name: "ocho" },
+  { value: 9, name: "nueve" },
+  { value: 10, name: "diez" },
+  { value: 11, name: "jota" },
+  { value: 12, name: "reina" },
+  { value: 13, name: "rey" },
 ];
 
 const deck = [];
 
-const totalDeck = (palo) => {
+const totalDeck = (suit) => {
   for (i = 0; i < 13; i++) {
     deck.push({
-      symbol: palo,
-      valor: carta[i].valor,
+      symbol: suit,
+      valor: carta[i].value,
       name: carta[i].name,
     });
   }
@@ -61,3 +71,13 @@ const playGame = () => {
 //3. Qué hace dentro? Qué responsabilidades tiene?
 
 playGame();
+
+const displaynNumber = () => 
+  playerCard.innerText= ;
+  playerCard = myCard.valor;
+
+
+restart.addEventListener("click", () => {
+  counterDisplay = 0;
+  counter.textContent = counterDisplay;
+});
